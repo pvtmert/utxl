bdec3347ed38fcb429b8577f24ac8b46f32d0d92
Omar Albeik <omaralbeik@gmail.com>
2016-03-24 15:20:47 +0200

    initial commit: switched to visual studio 2015

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UTXL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // this method will be called when open buttun is clicked
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open File";
+            ofd.Filter = "Text Files (.txt)|*.txt|CSS Files (.css)|*.css";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show(ofd.SafeFileName);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using Gtk;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace UTXL
 {
-	class MainClass
-	{
-		public static string verstr;
-		public static void Main (string[] args)
-		{
-			verstr = "";
-			Application.Init ();
-			Gtk.Settings.Default.SetLongProperty ("gtk-menu-images", 1, "");
-			Gtk.Settings.Default.SetLongProperty ("gtk-button-images", 1, "");
-			Gtk.Settings.Default.SetLongProperty ("gtk-application-prefer-dark-theme", 1, "");
-			Gtk.Settings.Default.SetStringProperty ("gtk-icon-theme-name", "Tango", "");
-			Gtk.Settings.Default.SetStringProperty ("gtk-toolbar-style", "GTK_TOOLBAR_ICONS", "");
-			main win = new main ();
-			win.Show ();
-			Application.Run ();
-		}
-	}
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
 }
